72d3bcf75c4382a9a8053545f3a37704a619dbeb
Shubham Arawkar <arawkar.shubham08@gmail.com>
2018-09-24 05:47:03 +0530

    game done, runs locally

---
+++ game-of-life-vue/src/components/Cell.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cell" :class="{live:alive}"> 
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Cell extends Vue {
+  public alive: boolean = false;
+
+  public toggle(): void{
+    this.alive = !this.alive;
+    console.log(this.alive);
+  }
+
+  public isAlive():boolean {
+    return this.alive;
+  }
+
+  public isDead():boolean{
+    return !this.alive;
+  }
+
+  public ressurect(): void{
+    this.alive = true;
+  }
+
+  public kill(): void {
+    this.alive = false;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cell {
+  height: 15px;
+  width: 15px;
+  border: 1px solid #ccc;
+  display: inline-block;
+}
+
+.live {
+  background-color: #42b983;
+}
+</style>
